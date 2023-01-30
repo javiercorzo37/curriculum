301dd2d2843de87d50fde68eddf0c9151017d5f9
javiercorzo37 <85262455+javiercorzo37@users.noreply.github.com>
2023-01-29 23:48:54 -0300

    proyecto
    se crea portfolio

---
+++ script/main.js
@@ -0,0 +1,39 @@
+let menuVisible = false;
+// ocultar o muestra el menu
+
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("wordpress");
+        habilidades[3].classList.add("comunicacion");
+        habilidades[4].classList.add("trabajo");
+        habilidades[5].classList.add("creatividad");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
